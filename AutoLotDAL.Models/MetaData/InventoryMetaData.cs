23ed36f0ff96c31baa8610d24848d3fdf788794f
Ehsan Moosavi <eh.moosavi@outlook.com>
2022-01-24 15:13:50 +0330

    frist commit

---
+++ AutoLotDAL.Models/MetaData/InventoryMetaData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace AutoLotDAL.Models.MetaData
+{
+    class InventoryMetaData
+    {
+        [Display(Name = "Pet Name")]
+        public String PetName;
+        [StringLength (50,ErrorMessage ="Please Enter A valu Less Than 50 chracters Long")]
+        public string Make;
+    }
+}
---
+++ CarLotMVC/Controllers/InventoryController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AutoLotDAL.EF;
+using AutoLotDAL.Models;
+using AutoLotDAL.Repos;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+
+namespace CarLotMVC.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private AutoLotEntities db = new AutoLotEntities();
+        private readonly InventoryRepo _repo = new InventoryRepo();
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return PartialView(_repo.GetAll());
+        }
+
+        // GET: Inventory/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Cars.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventory/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Make,Color,PetName,Timestamp")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cars.Add(inventory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inventory);
+        }
+
+        // GET: Inventory/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Cars.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Make,Color,PetName,Timestamp")] Inventory inventory)
+        {
+           
+            try
+            {
+                if (ModelState.IsValid) 
+              
+                {
+                    db.Entry(inventory).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+
+            }
+
+            catch(Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to create record: {ex.Message}");
+                return View(inventory);
+            }
+            return View(inventory);
+           
+        }
+
+        // GET: Inventory/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Cars.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventory inventory = db.Cars.Find(id);
+            db.Cars.Remove(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _repo.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+    }
+}
